2c8db74d4ef3aa9323d9508c6bbe5dc9b07b303a
Alessandro Martins <alessandro.f.martins@gmail.com>
2020-04-18 04:08:49 -0300

    Criada funcionalidade de linha de comando e configuracao via JSON

--- brazil_covid19.py
+++ brazil_covid19.py
@@ -2,83 +2,146 @@
 
 # %matplotlib
 
+from json import load as j_load
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 from scipy.optimize import curve_fit
-import fit_funcs
+from fit_funcs import fit_funcs
 
+# STATES = {'Acre': 'o', 'Alagoas': 'e', 'Amapá':'o', 'Amazonas':'o',
+#           'Bahia': 'a', 'Ceará':'o', 'Distrito Federal': 'o',
+#           'Espírito Santo':'o', 'Goiás':'e', 'Maranhão':'o',
+#           'Mato Grosso':'o', 'Mato Grosso do Sul':'o', 'Minas Gerais':'e',
+#           'Paraná':'o', 'Paraíba':'a', 'Pará':'o', 'Pernambuco':'o',
+#           'Piauí':'o', 'Rio Grande do Norte':'o',
+#           'Rio Grande do Sul':'o', 'Rio de Janeiro':'o', 'Rondônia':'e',
+#           'Roraima':'e', 'Santa Catarina':'e', 'Sergipe':'e',
+#           'São Paulo':'e', 'Tocantins':'o'}
 
-fit_func_cases = fit_funcs.fourth_func
-fit_func_dcases = fit_funcs.sixth_func
+with (open('./covid_config.json', 'r')) as cfg_file:
+    cfg = j_load(cfg_file)
+
 
 # Le os casos do arquivo CSV:
-covid = pd.read_csv('./brazil_covid19.csv')
-covid['date'] = pd.to_datetime(covid['date'])
-temp = covid[covid['state'] == 'São Paulo'].drop(
-    columns=['state', 'region']).reset_index(drop=True)
-covid_SP = pd.DataFrame(data=temp.drop(columns='date').values,
-                        index=temp['date'].values,
-                        columns=['cases', 'deaths'])
+def get_df():
+    df = pd.read_csv(cfg['INPUT_FILE'], sep=cfg['INPUT_FILE_SEP'])
+    df[cfg['DATE_COL']] = pd.to_datetime(df[cfg['DATE_COL']])
+    states = np.unique(df[cfg['STATE_COL']].values)
 
-today = covid_SP.index[-1]
+    return df, states
+
+
+# Seleciona o dado por Estado
+def get_state_data(df, state='São Paulo'):
+    temp = df[df[cfg['STATE_COL']] == state][
+        [cfg['DATE_COL']] + cfg['DATA_COLS']].reset_index(drop=True)
+    ret_df = pd.DataFrame(data=temp.drop(columns=cfg['DATE_COL']).values,
+                          index=temp[cfg['DATE_COL']].values,
+                          columns=cfg['DATA_COLS'])
+    today = ret_df.index[-1]
+
+    return ret_df, today
+
 
 # Gera as variações diárias de casos e mortes:
-delta_cases = [int(covid_SP.cases.iloc[i+1] - covid_SP.cases.iloc[i])
-               for i in range(covid_SP.cases.size - 1)]
-delta_cases.insert(0, 0)
-delta_deaths = [int(covid_SP.deaths.iloc[i+1] - covid_SP.deaths.iloc[i])
-                for i in range(covid_SP.deaths.size - 1)]
-delta_deaths.insert(0, 0)
-covid_SP['delta_cases'] = delta_cases
-covid_SP['delta_deaths'] = delta_deaths
+def generate_daily_delta(df):
+    for col in cfg['COLS_FOR_DELTA']:
+        delta_col = [int(df[col].iloc[i+1] - df[col].iloc[i])
+                     for i in range(df[col].size - 1)]
+        delta_col.insert(0, 0)
+        df['delta_' + col] = delta_col
+
+    return df
 
 
 # Gera as funções de projeção:
-day_number = np.array(range(covid_SP.cases.size))
+def generate_projection_function(df):
+    fit_cols = cfg['COLS_FOR_PROJECTION'].copy()
+    day_number = np.arange(df[cfg['COLS_FOR_PROJECTION'][0]['name']].size)
 
-popt_cases, _ = curve_fit(fit_func_cases, day_number,
-                          covid_SP.cases.values)
-projected_cases = [fit_func_cases(d, *popt_cases) for d in day_number]
+    for col in fit_cols:
+        coefs, _ = curve_fit(fit_funcs[col['fit_func']], day_number,
+                             df[col['name']].values)
+        df['projected_' + col['name']] = [fit_funcs[col['fit_func']](d, *coefs)
+                                          for d in day_number]
+        col['coefs'] = coefs
 
-popt_dcases, _ = curve_fit(fit_func_dcases, day_number,
-                           covid_SP.delta_cases.values)
-projected_dcases = [fit_func_dcases(d, *popt_dcases) for d in day_number]
+    return df, fit_cols
 
-covid_SP['projected_cases'] = projected_cases
-covid_SP['projected_dcases'] = projected_dcases
 
-# Cria a projeção para o próximo mês:
-added_days = np.array(range(day_number[-1] + 1, day_number[-1] + 31))
-added_cases = [fit_func_cases(d, *popt_cases) for d in added_days]
-added_dcases = [fit_func_dcases(d, *popt_dcases) for d in added_days]
+# Cria a projeção para o próximo período:
+def generate_next_period_projection(df, fit_cols, period=30):
+    first_period = df[cfg['COLS_FOR_PROJECTION'][0]['name']].size
+    added_days = np.arange(first_period, first_period + period + 1)
+    added_data = []
+    # cols = tuple(fit_cols.keys())
 
-for added_case, added_dcase in zip(added_cases, added_dcases):
-    covid_add = pd.DataFrame([[added_case, added_dcase]],
-                            columns=['projected_cases', 'projected_dcases'],
-                            index=[covid_SP.index[-1] +
-                                   timedelta(days=1)])
-    covid_SP = covid_SP.append(covid_add)           
+    for col in fit_cols:
+        added_data.append([fit_funcs[col['fit_func']](d, *col['coefs'])
+                           for d in added_days])
+
+    for added_d in zip(*added_data):
+        covid_add = pd.DataFrame(
+            [list(added_d)],
+            columns=['projected_' + c['name'] for c in fit_cols],
+            index=[df.index[-1] + timedelta(days=1)])
+        df = df.append(covid_add)
+
+    return df
+
 
 # Cria os gráficos:
-plt.plot_date(x=covid_SP.index, y=covid_SP.cases,
-              label='Casos', marker=',', ls='-')
-plt.plot_date(x=covid_SP.index, y=covid_SP['delta_cases'],
-              label='Variação diária dos casos', marker=',', ls='-')
-plt.plot_date(x=covid_SP.index, y=covid_SP.deaths,
-              label='Mortes', marker=',', ls='-')
-plt.plot_date(x=covid_SP.index, y=covid_SP['delta_deaths'],
-              label='Variação diária das mortes', marker=',', ls='-')
-plt.plot_date(x=covid_SP.index, y=covid_SP['projected_cases'],
-              label='Casos projetados', marker=',', ls='--')
-plt.plot_date(x=covid_SP.index, y=covid_SP['projected_dcases'],
-              label='Variação diária dos casos (projetado)',
-              marker=',', ls='--')
-plt.axvline(x=today, ls=':', c='m',
-            label='Hoje (%s)' % datetime.strftime(today, '%d/%m/%Y'))
+def plot_graphics(df, today, state, plot_projected=True):
+    plt.plot_date(x=df.index, y=df[cfg['DATA_COLS'][0]],
+                  label='Casos', marker=',', ls='-')
+    plt.plot_date(x=df.index, y=df['delta_' + cfg['COLS_FOR_DELTA'][0]],
+                  label='Variação diária dos casos', marker=',', ls='-')
+    plt.plot_date(x=df.index, y=df[cfg['DATA_COLS'][1]],
+                  label='Mortes', marker=',', ls='-')
+    plt.plot_date(x=df.index, y=df['delta_' + cfg['COLS_FOR_DELTA'][1]],
+                  label='Variação diária das mortes', marker=',', ls='-')
 
-plt.title('Casos de Covid-19 no Estado de São Paulo até %s' %
-          datetime.strftime(covid_SP.index[-1], '%d/%m/%Y'))
-plt.legend()
-plt.show()
+    _df = df.copy() if plot_projected else df[df.index[0]: today]
+    plt.plot_date(x=_df.index,
+                  y=_df['projected_' + cfg['COLS_FOR_PROJECTION'][0]['name']],
+                  label='Casos projetados', marker=',', ls='--')
+    plt.plot_date(x=_df.index,
+                  y=_df['projected_' + cfg['COLS_FOR_PROJECTION'][1]['name']],
+                  label='Variação diária dos casos (projetado)',
+                  marker=',', ls='--')
+    plt.axvline(x=today, ls=':', c='m',
+                label='Hoje (%s)' % datetime.strftime(today, '%d/%m/%Y'))
+
+    last_day = datetime.strftime(df.index[-1] if plot_projected else today,
+                                 '%d/%m/%Y')
+    plt.title('Casos de Covid-19 no Estado de %s até %s' % (state, last_day))
+    plt.legend()
+    plt.show()
+
+
+if __name__ == "__main__":
+
+    df, states = get_df()
+
+    while True:
+        state = input('Entre com o nome do Estado para análise (ou qualquer outra linha para sair): ')
+        if state not in states:
+            break
+
+        state_df, today = get_state_data(df, state)
+        state_df = generate_daily_delta(state_df)
+        state_df, fit_cols = generate_projection_function(state_df)
+
+        try:
+            use_proj = int(input('Projeção para quantos dias (0 para não gerar): '))
+        except ValueError:
+            use_proj = 0
+        if use_proj:
+            state_df = generate_next_period_projection(state_df,
+                                                       fit_cols, use_proj)
+        plot_graphics(state_df, today, state, use_proj != 0)
+        print('Seu gráfico foi gerado!')
+
+    print('Obrigado!')
--- fit_funcs.py
+++ fit_funcs.py
@@ -1,30 +1,16 @@
 import numpy as np
 
 
-def exp_func(x, a, b, c):
-    return a * np.exp(b * x) + c
-
-
-def lin_func(x, x1, x0):
-    return x1 * x + x0
-
-
-def quad_func(x, x2, x1, x0):
-    return x2 * x**2 + x1 * x + x0
-
-
-def third_func(x, x3, x2, x1, x0):
-    return x3 * x**3 + x2 * x**2 + x1 * x + x0
-
-
-def fourth_func(x, x4, x3, x2, x1, x0):
-    return x4 * x**4 + x3 * x**3 + x2 * x**2 + x1 * x + x0
-
-
-def sixth_func(x, x6, x5, x4, x3, x2, x1, x0):
-    return x6 * x**6 + x5 * x**5 + x4 * x**4 + x3 * x**3 \
-        + x2 * x**2 + x1 * x + x0
-
-
-def dummy_func1(x, x3, x2, x1, x0):
-    return x3 * np.sin(x2 * x) + x1 * x + x0
+fit_funcs = {
+    'poly_1': lambda x, x1, x0: x1 * x + x0,
+    'poly_2': lambda x, x2, x1, x0: x2 * x**2 + x1 * x + x0,
+    'poly_3': lambda x, x3, x2, x1, x0: x3 * x**3 + x2 * x**2 + x1 * x + x0,
+    'poly_4': lambda x, x4, x3, x2, x1, x0: x4 * x**4 + x3 * x**3 + x2 * x**2 +
+    x1 * x + x0,
+    'poly_5': lambda x, x5, x4, x3, x2, x1, x0: x5 * x**5 + x4 * x**4 +
+    x3 * x**3 + x2 * x**2 + x1 * x + x0,
+    'poly_6': lambda x, x6, x5, x4, x3, x2, x1, x0: x6 * x**6 + x5 * x**5 + x4
+    * x**4 + x3 * x**3 + x2 * x**2 + x1 * x + x0,
+    'exp': lambda x, a, b, c: a * np.exp(b * x) + c,
+    'dummy_1': lambda x, x3, x2, x1, x0: x3 * np.sin(x2 * x) + x1 * x + x0
+}
